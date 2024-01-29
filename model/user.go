4a7af66b7ebe94c1588201de6f0524ecafac5871
RonexLemon <ronexondimu@gmail.com>
2024-01-29 08:32:08 +0300

    validation

--- model/user.go
+++ model/user.go
@@ -35,3 +35,16 @@ func (user *User) BeforeSave(*gorm.DB)error{
 	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
 	return nil
 }
+
+func (user *User) ValidatePassword(password string) error{
+	return bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(password))
+}
+func FindUserByUsername(username string)(User,error){
+	var user User
+	err:=database.Database.Where("username=?",username).Find(&user).Error
+
+	if err !=nil{
+		return user,nil
+	}
+	return user, nil
+}
